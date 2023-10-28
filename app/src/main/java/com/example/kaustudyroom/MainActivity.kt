c4739e8df8195ce90a9ddc5f862a5a1dfbf780fc
seplaz99 <101307190+seplaz99@users.noreply.github.com>
2023-10-28 20:41:38 +0900

    fragment + navigation

--- app/src/main/java/com/example/kaustudyroom/MainActivity.kt
+++ app/src/main/java/com/example/kaustudyroom/MainActivity.kt
@@ -1,27 +1,44 @@
 package com.example.kaustudyroom
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.kaustudyroom.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        binding.btnSwitchTimetablePage.setOnClickListener{
-            binding.txtConfirm.text = "타임테이블 페이지로 이동합니다."
-            val nextIntent = Intent(this, studyroomTimeTableActivity::class.java)
-            startActivity(nextIntent)
-        }
-        binding.btnSwitchPicturePage.setOnClickListener{
-            binding.txtConfirm.text = "입실확인 및 벌점조회 페이지로 이동합니다."
-            val nextIntent = Intent(this, PointActivity::class.java)
-            startActivity(nextIntent)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val navController = binding.frgStudyroom.getFragment<NavHostFragment>().navController
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.mainFragment, R.id.studyRoomFragment, R.id.pointFragment, R.id.reservationConfirmFragment)
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.bottomNav.setupWithNavController(navController)
+        setContentView(binding.root)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = binding.frgStudyroom.getFragment<NavHostFragment>().navController
+
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    fun hideBottomNavigation(bool: Boolean) {
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_nav)
+        if (bool) {
+            bottomNavigation.visibility = View.GONE
+        } else {
+            bottomNavigation.visibility = View.VISIBLE
         }
     }
 }
--- app/src/main/java/com/example/kaustudyroom/PointAdapter.kt
+++ app/src/main/java/com/example/kaustudyroom/PointAdapter.kt
@@ -5,7 +5,6 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kaustudyroom.databinding.ListPointBinding
 
-
 class PointAdapter(val point: Array<Point>): RecyclerView.Adapter<PointAdapter.Holder>() {
     // ViewHolder 객체를 생성하고 초기화
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PointAdapter.Holder {
