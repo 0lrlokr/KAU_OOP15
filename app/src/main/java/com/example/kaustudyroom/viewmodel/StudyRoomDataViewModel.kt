3cc71004192d5ebd9674b610aead60c34991ddd4
ieunju <gleunju217@gmail.com>
2023-12-05 00:40:32 +0900

    addUserReservedData - kyh ( feat. lej )

--- app/src/main/java/com/example/kaustudyroom/viewmodel/StudyRoomDataViewModel.kt
+++ app/src/main/java/com/example/kaustudyroom/viewmodel/StudyRoomDataViewModel.kt
@@ -261,7 +261,7 @@ class StudyRoomDataViewModel: ViewModel() {
             checkInNPointNode.setValue(
                 CheckInVO(
                     checkedInTime,
-                    point
+                    point,
                 )
             )
         }
